fff954453c98ed4a04cbe38e336479affb37736d
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-14 17:35:38 +0330

    server connection between server and client

--- chat-app/client.js
+++ chat-app/client.js
@@ -1,3 +1,9 @@
 const net = require("net");
 
-const server = net.createConnection();
+const client = net.createConnection({ port: 3008, host: "127.0.0.1" }, () => {
+  console.log("connected to the server!");
+});
+
+client.on("end", () => {
+  console.log("Connection was ended!");
+});
--- chat-app/server.js
+++ chat-app/server.js
@@ -1,3 +1,11 @@
 const net = require("net");
 
 const server = net.createServer();
+
+server.on("connection", (socket) => {
+  console.log("a new connection to the server!");
+});
+
+server.listen(3008, "127.0.0.1", () => {
+  console.log(`opened server on ${JSON.stringify(server.address())}`);
+});
