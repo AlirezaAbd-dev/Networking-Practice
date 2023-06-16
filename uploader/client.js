980a1d6bdcde7c483e5130917acf619056cb0232
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-16 23:06:59 +0330

    select filePath option added...

--- uploader/client.js
+++ uploader/client.js
@@ -1,10 +1,15 @@
 const net = require("net");
 const fs = require("node:fs/promises");
+const path = require("path");
+const { argv } = require("process");
 
 const socket = net.createConnection({ port: 5050, host: "::1" }, async () => {
-  const filePath = "./text.txt";
+  const filePath = argv[2];
+  const fileName = path.basename(filePath);
   const fileHandle = await fs.open(filePath, "r");
   const fileReadStream = fileHandle.createReadStream();
+
+  socket.write(`fileName: ${fileName}-------`);
 
   //   Readeing from the source file
   fileReadStream.on("data", (data) => {
--- uploader/server.js
+++ uploader/server.js
@@ -11,11 +11,23 @@ server.on("connection", async (socket) => {
   socket.on("data", async (data) => {
     let buff = true;
     if (!fileHandle) {
-      fileHandle = await fs.open("storage/test.txt", "w");
+      // Pause the reading until the file is open then resume
+      // Because the data will be recieved one after the other and the file isn't open yet
+      socket.pause();
+
+      const indexOfDivider = data.indexOf("-------");
+      const fileName = data.subarray(10, indexOfDivider).toString("utf-8");
+
+      fileHandle = await fs.open(`storage/${fileName}`, "w");
       fileWriteStream = fileHandle.createWriteStream();
 
+      const indexOfData = data.indexOf("-------") + 7;
+
       // Writing to our destination file
-      buff = fileWriteStream.write(data);
+      buff = fileWriteStream.write(data.subarray(indexOfData));
+
+      // So we pause reading and after file opened we resume reading
+      socket.resume();
     } else {
       buff = fileWriteStream.write(data);
     }
